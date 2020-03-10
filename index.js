1dd50b2d7298af34562a098ccebd3d867f19eb3e
Schow94 <Schow94@gmail.com>
2020-03-09 18:43:04 -0700

    Add Diagonal Scoring

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   console.log('DOM has loaded');
-
+  var moves = 0;
   //WINNING SCENARIOS
   //Horizontal Winning Scenarios (3)
   var winOne = [['X', 'X', 'X'], [], []];
@@ -17,89 +17,132 @@ $(document).ready(function() {
   var turn = 'X';
   var winner;
 
-  //Checks if you can win horizontally
-  //Accounts for 3/8 possible solutions
   //Better to have 1 fxn that checks for all winning scenarios
-  const checkHorizontally = arr => {
-    let arrOne = arr[0];
-    let arrTwo = arr[1];
-    let arrThree = arr[2];
-
-    //Checks 1 arr
-    const checkRow = nestedArr => {
-      var xCount = 0;
-      var oCount = 0;
-
-      for (let i = 0; i < nestedArr.length; i++) {
-        // console.log(nestedArr[i]);
-        if (nestedArr[i] === 'X') xCount++;
-        else if (nestedArr[i] === 'O') oCount++;
-      }
-      if (xCount === 3) {
-        winner = 'playerOne';
-      } else if (oCount === 3) {
-        winner = 'playerTwo';
-      }
-      // console.log(xCount, oCount);
-    };
-
-    //Run 3 times to check 3 nested arrays
-    checkRow(arrOne);
-    checkRow(arrTwo);
-    checkRow(arrThree);
-    // console.log('winner: ', winner);
-  };
-
-  //Check for a vertical win
-  //Clean up - Not DRY at all
-  //This fxn doesn't know who won - need access
-  const checkVertically = arr => {
-    const checkColumnOne = () => {
+  const checkForWin = data => {
+    //Checks if you can win horizontally
+    //Accounts for 3/8 possible solutions
+    const checkHorizontally = arr => {
       let arrOne = arr[0];
       let arrTwo = arr[1];
       let arrThree = arr[2];
 
-      var top = arrOne[0];
-      var middle = arrTwo[0];
-      var bottom = arrThree[0];
+      //Checks 1 arr
+      const checkRow = nestedArr => {
+        var xCount = 0;
+        var oCount = 0;
 
-      console.log(top, middle, bottom);
-      if (top === middle && bottom === middle) {
-        console.log('vertical win');
-      }
+        for (let i = 0; i < nestedArr.length; i++) {
+          if (nestedArr[i] === 'X') xCount++;
+          else if (nestedArr[i] === 'O') oCount++;
+        }
+        if (xCount === 3) {
+          winner = 'playerOne';
+        } else if (oCount === 3) {
+          winner = 'playerTwo';
+        }
+      };
+      //Run 3 times to check 3 nested arrays
+      checkRow(arrOne);
+      checkRow(arrTwo);
+      checkRow(arrThree);
     };
 
-    const checkColumnTwo = () => {
-      let arrOne = arr[0];
-      let arrTwo = arr[1];
-      let arrThree = arr[2];
+    //Check for a vertical win
+    //Clean up - Not DRY at all
+    const checkVertically = arr => {
+      var arrOne = arr[0];
+      var arrTwo = arr[1];
+      var arrThree = arr[2];
 
-      var top = arrOne[1];
-      var middle = arrTwo[1];
-      var bottom = arrThree[1];
+      const checkColumnOne = () => {
+        var top = arrOne[0];
+        var middle = arrTwo[0];
+        var bottom = arrThree[0];
 
-      console.log(top, middle, bottom);
-      if (top === middle && bottom === middle) {
-        console.log('vertical win');
-      }
+        if (top === 'X' && top === middle && bottom === middle) {
+          winner = 'playerOne';
+        } else if (top === 'O' && top === middle && bottom === middle) {
+          winner = 'playerTwo';
+        }
+      };
+
+      const checkColumnTwo = () => {
+        var top = arrOne[1];
+        var middle = arrTwo[1];
+        var bottom = arrThree[1];
+
+        if (top === 'X' && top === middle && bottom === middle) {
+          winner = 'playerOne';
+        } else if (top === 'O' && top === middle && bottom === middle) {
+          winner = 'playerTwo';
+        }
+      };
+
+      const checkColumnThree = () => {
+        var top = arrOne[2];
+        var middle = arrTwo[2];
+        var bottom = arrThree[2];
+
+        if (top === 'X' && top === middle && bottom === middle) {
+          winner = 'playerOne';
+        } else if (top === 'O' && top === middle && bottom === middle) {
+          winner = 'playerTwo';
+        }
+      };
+      checkColumnOne();
+      checkColumnTwo();
+      checkColumnThree();
     };
-    const checkColumnThree = () => {
-      let arrOne = arr[0];
-      let arrTwo = arr[1];
-      let arrThree = arr[2];
 
-      var top = arrOne[2];
-      var middle = arrTwo[2];
-      var bottom = arrThree[2];
+    const checkDiagonally = data => {
+      //First Diagonal Scenario
+      const leftToRight = arr => {
+        let temp = [];
+        //Create temp arr to store diagonals
+        for (let i = 0; i < arr.length; i++) {
+          temp.push(arr[i][i]);
+        }
 
-      console.log(top, middle, bottom);
-      if (top === middle && bottom === middle) {
-        console.log('vertical win');
-      }
+        if (temp[0] === 'X' && temp[1] === temp[0] && temp[2] === temp[0]) {
+          winner = 'playerOne';
+          console.log(winner);
+        } else if (
+          temp[0] === 'O' &&
+          temp[1] === temp[0] &&
+          temp[2] === temp[0]
+        ) {
+          winner = 'playerTwo';
+        }
+      };
+
+      //Second Diagonal Scenario
+      const rightToLeft = arr => {
+        let temp = [];
+        //Create temp arr to store diagonals
+        for (let i = 0; i < arr.length; i++) {
+          temp.push(arr[i][arr.length - 1 - i]);
+        }
+        //Check temp arr to see if all values are same
+        if (temp[0] === 'X' && temp[1] === temp[0] && temp[2] === temp[0]) {
+          winner = 'playerOne';
+          console.log(winner);
+        } else if (
+          temp[0] === 'O' &&
+          temp[1] === temp[0] &&
+          temp[2] === temp[0]
+        ) {
+          winner = 'playerTwo';
+        }
+      };
+
+      leftToRight(data);
+      rightToLeft(data);
+      // console.log('Checking Diagonally');
     };
-    checkColumnOne();
-    checkColumnTwo();
-    checkColumnThree();
+
+    checkHorizontally(data);
+    checkVertically(data);
+    checkDiagonally(data);
   };
 
   //Selectors
@@ -107,7 +150,7 @@ $(document).ready(function() {
   $cell = $('.cell');
   $message = $('.message');
 
-  //Not sure how to update this array when HTMLis updated
+  // Keep track of score separately in an array
   var boardArr = [
     [null, null, null],
     [null, null, null],
@@ -115,6 +158,7 @@ $(document).ready(function() {
   ];
 
   $rowsContainer.on('click', $cell, e => {
+    moves++;
     console.log('turn: ', turn);
     var clickedCell = $(e.target)[0].className;
     //Converts a cell className into an arr to get coordinates
@@ -137,12 +181,16 @@ $(document).ready(function() {
         .eq(0)
         .text('X');
 
-      clickedCellText.addClass('playerOne');
+      clickedCellText
+        .children()
+        .eq(0)
+        .addClass('playerOne');
 
       boardArr[coordOne][coordTwo] = 'X';
       //Check score to see if someone won
-      checkHorizontally(boardArr);
-      checkVertically(boardArr);
+      // checkHorizontally(boardArr);
+      // checkVertically(boardArr);
+      checkForWin(boardArr);
 
       //Change message so player knows when to go
       $message.text("Player Two's Turn");
@@ -157,12 +205,16 @@ $(document).ready(function() {
         .eq(0)
         .text('O');
 
-      clickedCellText.addClass('playerTwo');
+      clickedCellText
+        .children()
+        .eq(0)
+        .addClass('playerTwo');
 
       boardArr[coordOne][coordTwo] = 'O';
       //Check score to see if someone won
-      checkHorizontally(boardArr);
-      checkVertically(boardArr);
+      // checkHorizontally(boardArr);
+      // checkVertically(boardArr);
+      checkForWin(boardArr);
 
       //Change msg so player knows when to go
       $message.text("Player One's Turn");
@@ -186,5 +238,13 @@ $(document).ready(function() {
     //   // $($cell[i]).text(`${boardArr[i]}`);
     //   console.log(boardArr[i], $cell[i]);
     // }
+
+    console.log('moves: ', moves);
+    // No Winner
+    if (moves === 9 && !winner) {
+      console.log('DRAW');
+    }
+    //Disable clicking once someone wins
+    //Disable clicking on tiles that have been clicked on already
   });
 });
